2b38b1b834a40f74680cbf978236277319c5b30d
Tea <tea@null.net>
2024-08-22 17:02:30 +0100

    using chi for server setup now!!

--- server.go
+++ server.go
@@ -2,14 +2,32 @@ package main
 
 import (
 	"net/http"
+	"time"
 
-	"github.com/labstack/echo/v4"
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
 )
 
 func main() {
-	e := echo.New()
-	e.GET("/", func(c echo.Context) error {
-		return c.String(http.StatusOK, "Hello, World!")
+	rtr := chi.NewRouter()
+
+	rtr.Use(middleware.RequestID)
+	rtr.Use(middleware.RealIP)
+	rtr.Use(middleware.Logger)
+	rtr.Use(middleware.Recoverer)
+
+  	// Set a timeout value on the request context (ctx), that will signal
+  	// through ctx.Done() that the request has timed out and further
+  	// processing should be stopped.
+  	rtr.Use(middleware.Timeout(60 * time.Second))
+
+	rtr.Get("/dog", func(w http.ResponseWriter, req *http.Request) {
+		
 	})
-	e.Logger.Fatal(e.Start(":7007"))
+
+
+
+
+
+	http.ListenAndServe(":7447", rtr)
 }
